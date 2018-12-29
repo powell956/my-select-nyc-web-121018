9ca23dd4bd01aa6cc1c6ddb5f2a85fc76ca4a92d
powell956 <powell956@gmail.com>
2018-12-29 03:03:35 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,8 +2,8 @@ def my_select(collection)
  arr = []
  counter = 0
 
- while counter < arr.length
-   if yield(collection[counter])
+ while counter < collection.length
+   if yield == collection[counter]
      arr << yield
    end
  end
